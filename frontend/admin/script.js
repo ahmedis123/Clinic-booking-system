27834c520804fb85647ddb0939f5f8f29148c6c8
ahmedis123 <68246919+ahmedis123@users.noreply.github.com>
2025-01-25 20:59:23 +0200

    Update script.js

--- frontend/admin/script.js
+++ frontend/admin/script.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3001/api';
+const API_URL = 'https://clinic-booking-system-opre.onrender.com:3001/api';
 
 document.addEventListener('DOMContentLoaded', loadAppointments);
 
